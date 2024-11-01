a05d4ed753ab3aba320063a3ac29993f6931f6f7
duda671 <eduardaaraujorod@gmail.com>
2024-11-01 10:22:21 -0300

    commit 1

--- src/main/java/saude/api/api/model/Exercicio.java
+++ src/main/java/saude/api/api/model/Exercicio.java
@@ -1,12 +1,17 @@
 package saude.api.api.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Exercicio {
 
     @Id
